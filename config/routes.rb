615b7d3ffae04a3b6ce0414dabaa825e23c833c5
LoganMai0112 <logan.mai.goldenowl@gmail.com>
2022-12-09 17:47:31 +0700

    finish sign in, signup and log out function

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'posts#index'
   devise_for :users
-  root 'posts#index'
   resources :posts
-
-  get 'users/login', to: 'users#login', as: 'login_user'
-  get 'users/signup', to: 'users#new', as: 'new_user'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
