af3e104b3b2edcf98d01ce6f30b65dab76bb8cba
LoganMai0112 <logan.mai.goldenowl@gmail.com>
2022-12-12 14:22:26 +0700

    fix avatar button

--- app/javascript/packs/login.js
+++ app/javascript/packs/login.js
@@ -1,14 +1,16 @@
 document.addEventListener("turbolinks:load", () => {
   var button = document.getElementById("drop-button");
-  var menu = document.getElementById("menu");
   button.addEventListener("click", function (event) {
-    if (menu.style.display == "none") {
-      menu.style.display = "flex";
-    } else {
+    var menu = document.getElementById("menu");
+    console.log(menu);
+    if (menu.style.display == "flex") {
       menu.style.display = "none";
+    } else {
+      menu.style.display = "flex";
     }
   });
   window.addEventListener("click", function (event) {
+    var menu = document.getElementById("menu");
     if (
       !menu.contains(event.target) &&
       !button.contains(event.target) &&
