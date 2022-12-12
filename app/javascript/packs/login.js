fb1fddb549c7b1dcbc2f087a0c2e43dfdbcaf14b
LoganMai0112 <logan.mai.goldenowl@gmail.com>
2022-12-12 15:24:15 +0700

    delete comment

--- app/javascript/packs/login.js
+++ app/javascript/packs/login.js
@@ -1,25 +1,3 @@
-// document.addEventListener("turbolinks:load", () => {
-//   var button = document.getElementById("drop-button");
-//   button.addEventListener("click", function (event) {
-//     var menu = document.getElementById("menu");
-//     console.log(menu);
-//     if (menu.style.display == "flex") {
-//       menu.style.display = "none";
-//     } else {
-//       menu.style.display = "flex";
-//     }
-//   });
-//   window.addEventListener("click", function (event) {
-//     var menu = document.getElementById("menu");
-//     if (
-//       !menu.contains(event.target) &&
-//       !button.contains(event.target) &&
-//       menu.style.display == "flex"
-//     ) {
-//       menu.style.display = "none";
-//     }
-//   });
-// });
 $(document).on("turbolinks:load", function () {
   $("#drop-button").on("click", function () {
     if ($("#menu").is(":visible")) {
@@ -31,7 +9,6 @@ $(document).on("turbolinks:load", function () {
 
   $(window).on("click", function (e) {
     var button = $("#drop-button");
-    // console.log(button.has(e.target));
     if (button !== e.target && !button.has(e.target).length) {
       $("#menu").hide();
     }
