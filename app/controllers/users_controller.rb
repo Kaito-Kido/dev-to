eb838d389040146c5170b825fb1f0ae780d2af7d
LoganMai0112 <logan.mai.goldenowl@gmail.com>
2022-12-13 17:34:06 +0700

    edit and view profile, view newfeed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,5 +9,22 @@ class UsersController < ApplicationController
     @posts = @user.posts
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path
+    else
+      render :edit
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :avatar)
+    end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   devise_for :users
   resources :posts
 
-  get "/users/profile/:id", to: "users#show", as: 'profile_user'
+  get "/users/profile/:id", to: "users#show", as: 'user'
+  get "/users/:id/edit/", to: "users#edit", as: 'edit_user'
+  patch "/users/:id", to: "users#update", as: 'update_user'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
