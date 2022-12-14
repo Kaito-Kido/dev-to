ae12d59e038262804bc7e19936cda7d81ca6f72a
LoganMai0112 <logan.mai.goldenowl@gmail.com>
2022-12-14 09:51:25 +0700

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   root to: 'posts#index'
-  devise_for :users
+  devise_for :users, path_names: {
+    registration: 'register'
+}
   resources :posts
-
-  get "/users/profile/:id", to: "users#show", as: 'user'
-  get "/users/:id/edit/", to: "users#edit", as: 'edit_user'
-  patch "/users/:id", to: "users#update", as: 'update_user'
+  resources :users
+  # get "/users/profile/:id", to: "users#show", as: 'user'
+  # get "/users/:id/edit/", to: "users#edit", as: 'edit_user'
+  # patch "/users/:id", to: "users#update", as: 'update_user'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
