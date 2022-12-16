64b472f80eac04c08c6187ae76fac6c14debbde7
LoganMai0112 <logan.mai.goldenowl@gmail.com>
2022-12-16 10:19:28 +0700

    fix avtar

--- app/helpers/reacts_helper.rb
+++ app/helpers/reacts_helper.rb
@@ -1,7 +1,6 @@
 module ReactsHelper
   def reacted?(post)
     if user_signed_in?
-      # React.where(post: post, user: current_user).exists?
       post.reacters.include?(current_user)
     end
   end
