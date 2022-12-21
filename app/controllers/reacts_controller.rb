37b60a1521da76e5ea8e5fcaf3047c5bfed3953e
LoganMai0112 <logan.mai.goldenowl@gmail.com>
2022-12-21 16:44:48 +0700

    edit review

--- app/controllers/reacts_controller.rb
+++ app/controllers/reacts_controller.rb
@@ -5,21 +5,20 @@ class ReactsController < ApplicationController
     return if reacted?(params[:post_id])
 
     @react = React.new(user_id: current_user.id, post_id: params[:post_id])
+    @react.save
     respond_to do |format|
-      if @react.save
-        format.js 
-      end
+      format.js 
     end
   end
 
   def destroy
     if reacted?(params[:post_id])
       @react = React.find_by(post_id: params[:post_id], user_id: current_user.id)
+      @react.destroy
       respond_to do |format|
-        if @react.destroy
-          format.js {
-          }
-        end
+        format.js {
+        }
+
       end
     end
   end
