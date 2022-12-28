4c131857233550f4b727f430775ebb628d332ebd
LoganMai0112 <logan.mai.goldenowl@gmail.com>
2022-12-28 11:54:41 +0700

    auto save

--- app/javascript/packs/edit_post.js
+++ app/javascript/packs/edit_post.js
@@ -1,24 +1,82 @@
 $(document).on("turbo:load", function () {
   $("#cover-button").on("click", function () {
-    console.log(1);
     $("#file-field").trigger("click");
   });
 
   var typingTimer;
-  var doneTypingInterval = 3000;
+  var doneTypingInterval = 10000;
 
-  $("#content-field").on("blur", function () {
-    doneTyping();
+  // Catch attach and remove attach event
+  $("body").on("trix-attachment-add", function () {
+    clearTimeout(typingTimer);
+    typingTimer = setTimeout(doneTyping, doneTypingInterval);
   });
-  $("#file-field").on("blur", function () {
-    doneTyping();
-  });
-  $("#post-title").on("blur", function () {
-    doneTyping();
+  $("body").on("trix-attachment-remove", function () {
+    clearTimeout(typingTimer);
+    typingTimer = setTimeout(doneTyping, doneTypingInterval);
   });
 
+  // catching typing event on editor for auto save
+  $("#content-field").on("keydown", function () {
+    clearTimeout(typingTimer);
+  });
+  $("#content-field").on("keyup", function () {
+    clearTimeout(typingTimer);
+    typingTimer = setTimeout(doneTyping, doneTypingInterval);
+  });
+
+  // catching typing event on title editor for auto save
+  $("#post-title").on("keyup", function (e) {
+    clearTimeout(typingTimer);
+    typingTimer = setTimeout(doneTyping, doneTypingInterval);
+  });
+  $("#post-title").on("keydown", function () {
+    clearTimeout(typingTimer);
+  });
+
+  // catching change on cover for autosave
+  $("#file-field").on("change", function () {
+    readURL(this);
+    clearTimeout(typingTimer);
+    typingTimer = setTimeout(doneTyping, doneTypingInterval);
+  });
   function doneTyping() {
     $("#autosave").trigger("click");
-    console.log(1);
   }
+
+  // Control + s to save
+
+  $("#post-container-title").on("dragover", function (e) {
+    e.preventDefault();
+    e.stopPropagation();
+  });
+  $("#post-container-title").on("dragenter", function (e) {
+    e.preventDefault();
+    e.stopPropagation();
+  });
+
+  // $("#post-container-title").on("drop", function (e) {
+  //   if (
+  //     e.originalEvent.dataTransfer &&
+  //     e.originalEvent.dataTransfer.files.length
+  //   ) {
+  //     e.preventDefault();
+  //     e.stopPropagation();
+  //     /*UPLOAD FILES HERE*/
+
+  //     // $("#file-field").value = e.originalEvent.dataTransfer.files;
+  //   }
+  // });
 });
+
+function readURL(input) {
+  if (input.files && input.files[0]) {
+    var reader = new FileReader();
+
+    reader.onload = function (e) {
+      $("#cover-prev").attr("src", e.target.result);
+    };
+
+    reader.readAsDataURL(input.files[0]);
+  }
+}
