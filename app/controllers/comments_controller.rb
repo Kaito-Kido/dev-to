310f1258eac29ce93f3533b43f7bb380fedf0da5
LoganMai0112 <logan.mai.goldenowl@gmail.com>
2023-01-04 12:11:50 +0700

    fix

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,6 @@
 class CommentsController < ApplicationController
   before_action :find_commentable
+  before_action :find_comment, except: [:create]
   before_action :find_post, only: [:create, :update]
   before_action :authenticate_user!
 
@@ -11,28 +12,18 @@ class CommentsController < ApplicationController
   end
 
   def destroy
-    comment = Comment.find(params[:id])
-    if params[:postId]
-      post = Post.find(params[:postId])
-    elsif params[:post_id]
-      post = Post.find(params[:post_id])
-    end
-    if comment.destroy
-      redirect_to post_path(post)
+    @post = Post.find(params[:post_id])
+    if @comment.destroy
+      redirect_to post_path(@post)
     end
   end
 
   def edit
-    if params[:postId]
-      @postId = params[:postId]
-    elsif params[:post_id]
-      @postId = Post.find(params[:post_id]).id
-    end
-    @comment = Comment.find(params[:id])
+    @post = Post.find(params[:post_id])
+    @post_id = @post.id
   end
 
   def update
-    @comment = Comment.find(params[:id])
     @comment.assign_attributes(comment_params)
     if @comment.save
       redirect_to post_path(@post)
@@ -45,19 +36,20 @@ class CommentsController < ApplicationController
   end
 
   def find_commentable
-    if params[:comment_id]
-      @commentable = Comment.find(params[:comment_id])
-    elsif params[:post_id]
-      @commentable = Post.find(params[:post_id])
-    end
+    @commentable = 
+      if params[:comment_id]
+        Comment.find(params[:comment_id])
+      elsif params[:post_id]
+        Post.find(params[:post_id])
+      end
+  end
+
+  def find_comment
+    @comment = Comment.find(params[:id])
   end
 
   def find_post
-    if params[:post_id]
-      @post = Post.find(params[:post_id])
-    elsif params[:comment][:postId]
-      @post = Post.find(params[:comment][:postId])
-    end
+    @post = params[:post_id] ? Post.find(params[:post_id]) : Post.find(params[:comment][:post_id])
   end
   
 end
