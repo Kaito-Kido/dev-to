c87945168d195486c657d2e2d341268a837fab66
LoganMai0112 <logan.mai.goldenowl@gmail.com>
2023-01-10 09:41:14 +0700

    edit config active job

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,9 +11,9 @@ class User < ApplicationRecord
     has_many :posts, dependent: :destroy
     has_many :reacts, dependent: :destroy
     has_many :comments, dependent: :destroy
-    has_many :received_follows, foreign_key: :followed_id, class_name: Follow.name, dependent: :destroy
+    has_many :received_follows, foreign_key: :followed_id, class_name: "Follow", dependent: :destroy
     has_many :followers, through: :received_follows, source: :follower
-    has_many :given_follows, foreign_key: :follower_id, class_name: Follow.name, dependent: :destroy
+    has_many :given_follows, foreign_key: :follower_id, class_name: "Follow", dependent: :destroy
     has_many :followings, through: :given_follows, source: :followed_user
     has_many :send_notifications, foreign_key: :sender_id, class_name: Notification.name, dependent: :destroy
     has_many :notifications, foreign_key: :receiver_id, class_name: Notification.name, dependent: :destroy
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Rails.application.configure do
   # config.cache_store = :mem_cache_store
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
-  config.active_job.queue_name_prefix = "test_app_#{Rails.env}"
+  config.active_job.queue_name_prefix = "dev_to_production"
   config.active_job.queue_adapter = :sidekiq
 
   config.action_mailer.perform_caching = false
