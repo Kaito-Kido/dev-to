27431c81711c10a12787b2cfeee79bdb8f759713
Kaito-Kido <maidailam2001@gmail.com>
2023-02-13 01:08:46 +0700

    fix seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,65 +13,67 @@
 #   )
 # end
 
-# 100.times.each do |id|
-#   User.create!(
-#     name: Faker::Name.name,
-#     role: :user,
-#     email: Faker::Internet.email,
-#     password: Faker::Internet.password,
-#   )
-# end
+100.times.each do |id|
+  User.create!(
+    name: Faker::Name.name,
+    role: :user,
+    email: Faker::Internet.email,
+    password: Faker::Internet.password,
+  )
+end
+
+
 
 # Create post
-# user_ids = User.pluck(:id)
-# 1000.times.each do |id|
-#   Post.create(
-#     user_id: user_ids.sample, 
-#     title: Faker::Lorem.sentence,
-#     content: Faker::Lorem.sentence,
-#     status: published
-#   )
-# end
+user_ids = User.pluck(:id)
+1000.times.each do |id|
+  Post.create(
+    user_id: user_ids.sample, 
+    title: Faker::Lorem.sentence,
+    content: Faker::Lorem.sentence,
+    status: "published"
+  )
+end
 
 
 
 # seed avatar with active storate
-# users = User.all
-# users.each do |user|
-#   image = URI.parse("https://avatars.dicebear.com/api/adventurer-neutral/#{user.id}.svg").open
-#   user.avatar.attach(io: image, filename: "user#{user.id}")
-# end
+users = User.all
+users.each do |user|
+  image = URI.parse("https://avatars.dicebear.com/api/adventurer-neutral/#{user.id}.svg").open
+  user.avatar.attach(io: image, filename: "user#{user.id}")
+end
 
 #seed location, work and bio for user
-# users = User.all
-# information = []
-# users.each do |user| 
-#   information.append(
-#     id: user.id,
-#     location: Faker::Address.city,
-#     work: Faker::Company.name,
-#     education: Faker::Educator.university,
-#     bio: Faker::Lorem.sentence,
-#     updated_at: Time.now,
-#     created_at: Time.now
-#   )
-# end
-# User.upsert_all(information)
+users = User.all
+information = []
+users.each do |user| 
+  information.append(
+    id: user.id,
+    location: Faker::Address.city,
+    work: Faker::Company.name,
+    education: Faker::Educator.university,
+    bio: Faker::Lorem.sentence,
+    updated_at: Time.now,
+    created_at: Time.now
+  )
+end
+User.upsert_all(information)
 
-# 30.times.each do
-#   Category.create(name: Faker::ProgrammingLanguage.name)
-# end
+30.times.each do
+  Category.create(name: Faker::ProgrammingLanguage.name)
+end
 
-# posts = Post.all
-# posts.each do |post|
-#   4.times.each do
-#     Tag.create(post_id: post.id, category_id: Category.find(rand(1...Category.all.count)).id)
-#   end
-# end
+posts = Post.all
+posts.each do |post|
+  4.times.each do
+    Tag.create(post_id: post.id, category_id: Category.find(rand(1...Category.all.count)).id)
+  end
+end
 
 
 #Add content to post
-# posts = Post.all
-# posts.each do |post|
-#   post.update(content: Faker::Lorem.paragraph(sentence_count: 10))
-# end
+posts = Post.all
+posts.each do |post|
+  post.update(content: Faker::Lorem.paragraph(sentence_count: 10))
+end
